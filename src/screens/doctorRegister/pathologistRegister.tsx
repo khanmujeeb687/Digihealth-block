58327c18144827fd84f66094e7827f3f5764f577
Alisha <alishajaryal@gmail.com>
2021-06-09 15:20:44 +0530

    Pathologist Registration

--- src/screens/doctorRegister/pathologistRegister.tsx
+++ src/screens/doctorRegister/pathologistRegister.tsx
@@ -8,41 +8,69 @@ const PathologistRegister = () => {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Request Access to digital report.
-                </p>
+
                 <a
                     className="App-link"
                     href="https://reactjs.org"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Search for report
+                    Pathologist Registration
                 </a>
-                <InputGroup style={{width: 500 , margin:10}}
+                <InputGroup style={{width: 300, margin:10}}
                             className="mb-3">
                     <FormControl
-                        placeholder="Patient's no"
+                        placeholder="Name"
                         aria-label="Amount (to the nearest dollar)"
                     />
                 </InputGroup>
-                <InputGroup style={{width: 500}}
+                <InputGroup style={{width: 300 , margin:10}}
                             className="mb-3">
-                    <FormControl style={{width:250}}
-                                 placeholder="Enter Duration"
+                    <FormControl
+                        placeholder="Email"
+                        aria-label="Amount (to the nearest dollar)"
+                    />
+                </InputGroup>
+                <InputGroup style={{width: 300 , margin:10}}
+                            className="mb-3">
+                    <FormControl
+                        placeholder="License no"
+                        aria-label="Amount (to the nearest dollar)"
+                    />
+                </InputGroup>
+                <InputGroup style={{width: 300 , margin:10}}
+                            className="mb-3">
+                    <FormControl
+                        placeholder="Contact no"
+                        aria-label="Amount (to the nearest dollar)"
+                    />
+                </InputGroup>
+
+                <InputGroup style={{width: 300}}
+                            className="mb-3">
+                    <FormControl style={{width:50}}
+                                 placeholder="Specify your type"
                                  aria-label="Recipient's username"
                                  aria-describedby="basic-addon2"
                     />
-                    <InputGroup.Text id="basic-addon2">Days</InputGroup.Text>
+
+
+
+                </InputGroup>
+                <InputGroup style={{width: 300}}
+                            className="mb-3">
+                    <FormControl style={{width:50}}
+                                 placeholder="Address(houseNo,street,pin)"
+                                 aria-label="Recipient's username"
+                                 aria-describedby="basic-addon2"
+                    />
+
+
+                    <InputGroup.Text id="basic-addon2">India</InputGroup.Text>
                 </InputGroup>
 
-                <InputGroup style ={{width: 115 }}
-                            className="mb-3">
-                    <InputGroup.Checkbox aria-label="Checkbox for following text input" />
-                    <FormControl
-                        placeholder = "forever"
-                        aria-label="Text input with checkbox" />
-                </InputGroup>
+
+
 
 
                 <Button variant="success">Submit</Button>
