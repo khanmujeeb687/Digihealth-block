1dc55ee8017b889f827986fc128f68419d44eceb
Alisha <alishajaryal@gmail.com>
2021-06-10 10:17:07 +0530

    name

--- src/screens/pathologistHome.tsx
+++ src/screens/pathologistHome.tsx
@@ -16,7 +16,7 @@ const PathologistHome = () => {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Doctor's Home
+                    pathologist's Home
                 </a>
 
 
