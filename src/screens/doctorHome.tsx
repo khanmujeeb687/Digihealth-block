4e96d286eb2776a9669fef7dfc1aa2cd7545d7d4
Alisha <alishajaryal@gmail.com>
2021-06-10 10:06:43 +0530

    Doctor's home page Tabview

--- src/screens/doctorHome.tsx
+++ src/screens/doctorHome.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from '../logo.svg';
+import {Tabs,Tab} from "react-bootstrap";
 
 
 const DoctorHome = () => {
@@ -8,17 +9,31 @@ const DoctorHome = () => {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                   Request Access to digital report.
-                </p>
+
                 <a
                     className="App-link"
                     href="https://reactjs.org"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Search for report
+                    Doctor's Home
                 </a>
+                <Tabs style= {{width:window.innerWidth-100}} defaultActiveKey="profile" id="uncontrolled-tab-example">
+                    <Tab eventKey="Who viewed" title="Who viewed">
+                        <p >
+                            first</p>
+                    </Tab>
+                    <Tab eventKey="Requests" title="Requests">
+                        <p>second</p>
+                    </Tab>
+                <Tab eventKey="Permitted" title="Permitted" >
+                    <p> third</p>
+                </Tab>
+                    <Tab eventKey="Reports" title="Reports" >
+                        <p> Fourth</p>
+                    </Tab>
+                </Tabs>
+
             </header>
         </div>
     );
