032c608bb3ef23cf950e978157ea37765424be74
mujeeb <khanmujeen687@gmail.com>
2021-06-10 08:57:43 +0530

    adding some fox

--- src/screens/doctorHome.tsx
+++ src/screens/doctorHome.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from '../logo.svg';
-import {Button, FormControl, InputGroup} from "react-bootstrap";
+
 
 const DoctorHome = () => {
     return (
@@ -19,34 +19,6 @@ const DoctorHome = () => {
                 >
                     Search for report
                 </a>
-                <InputGroup style={{width: 500 , margin:10}}
-                            className="mb-3">
-                    <FormControl
-                        placeholder="Patient's no"
-                        aria-label="Amount (to the nearest dollar)"
-                    />
-                </InputGroup>
-                <InputGroup style={{width: 500}}
-                    className="mb-3">
-                    <FormControl style={{width:250}}
-                                     placeholder="Enter Duration"
-                        aria-label="Recipient's username"
-                        aria-describedby="basic-addon2"
-                    />
-                    <InputGroup.Text id="basic-addon2">Days</InputGroup.Text>
-                </InputGroup>
-
-                <InputGroup style ={{width: 115 }}
-                    className="mb-3">
-                    <InputGroup.Checkbox aria-label="Checkbox for following text input" />
-                    <FormControl
-                        placeholder = "forever"
-                        aria-label="Text input with checkbox" />
-                </InputGroup>
-
-
-                <Button variant="success">Submit</Button>
-
             </header>
         </div>
     );
