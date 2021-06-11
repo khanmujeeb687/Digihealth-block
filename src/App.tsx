b948ab3906b1efe4d983e1c0452e2e9930a4a551
Alisha <alishajaryal@gmail.com>
2021-06-11 19:23:14 +0530

    firebase connected

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import {Route, Switch, withRouter} from 'react-router-dom';
@@ -11,11 +11,25 @@ import PathologistRegister from "./screens/doctorRegister/pathologistRegister";
 import PathologistHome from "./screens/pathologistHome";
 import UploadReport from "./screens/uploadReport";
 import Login from "./screens/login";
-
+import firebase from "firebase";
+import {Button} from "react-bootstrap";
+const firebaseConfig = {
+    apiKey: "AIzaSyAKDp1VR4KlqyYQOYcGSyboURK6xHU38Ek",
+    authDomain: "ereport-7fc81.firebaseapp.com",
+    projectId: "ereport-7fc81",
+    storageBucket: "ereport-7fc81.appspot.com",
+    messagingSenderId: "32963531288",
+    appId: "1:32963531288:web:0907bb27a6be716f1d696d",
+    measurementId: "G-JVSDF7NE7Y"
+};
 
 function App() {
+
+    useEffect(()=>{  firebase.initializeApp(firebaseConfig);},[]);
+
     return (
         <div>
+
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/pathologist" component={Pathologist} />
