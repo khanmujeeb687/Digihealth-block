ae63e57a565f66182c8223eeb1c794b0be2ea9b5
mujeeb <khanmujeen687@gmail.com>
2021-06-12 10:14:11 +0530

    nit

---
+++ src/utils/blockChainUtil.ts
@@ -0,0 +1,8 @@
+import { CID } from 'ipfs-http-client'
+
+export class BlockChainUtil {
+    static uploadFileToIPFS=async(files:any[])=>{
+        // const ipfs = ipfsClient({ host: 'localhost', port: '5002', protocol: 'http' });
+
+    }
+}
