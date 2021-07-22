5950fe371d6c0582855fed19b6769532cfdd5f48
mujeeb <khanmujeen687@gmail.com>
2021-07-22 11:39:20 +0530

    blockchain fix

--- src/utils/blockChainUtil.ts
+++ src/utils/blockChainUtil.ts
@@ -1,9 +1,15 @@
-const { create } = require('ipfs-http-client')
+import {AddResult} from "ipfs-core-types/src/root";
+
+const { create ,urlSource} = require('ipfs-http-client')
 
 export class BlockChainUtil {
-    static uploadFileToIPFS=async(files:any[])=>{
-        const client = create(new URL('http://127.0.0.1:5002'))
-        const { cid } = await client.add([...files], { progress: (prog:number) => console.log(`received: ${prog}`) });
-        return cid;
+    static uploadFileToIPFS=async(file:any)=>{
+        try{
+            const client = create(new URL('http://127.0.0.1:5002'))
+            const {cid} = await client.add(file, { progress: (prog:number) => console.log(`received: ${prog}`) });
+            return cid.toString();
+        }catch (e) {
+         console.log({e});
+        }
     }
 }
